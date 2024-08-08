f695b9fca3602e52e89045d31b721d257aba439f
harikrishna483 <36473279+harikrishna483@users.noreply.github.com>
2024-08-08 10:41:28 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <assert.h> 
-#include "pair_colours.h"
-#include "number_conversions.h"
-#include "pair_colour_data.h"
+#include "paircolours.h"
+#include "numberconversions.h"
+#include "pairscolourdata.h"
 
 
 void testNumberTocolourPair(int pairNumber,
